a5c53179234f6a3832c4aad7b669673f7d768c09
Aleksey Demchenko <ad@pesochin.net>
2024-06-28 01:06:48 +0300

    11.95: LaCarta: Error processing

--- src/components/pages/Menu/Menu.tsx
+++ src/components/pages/Menu/Menu.tsx
@@ -1,22 +1,37 @@
 import Headling from '../../Headling/Headling';
 import styles from './Menu.module.css';
 import Search from '../../Search/Search';
-import ProductCart from '../../ProductCart/ProductCart';
 import { API } from '../../../helpers/API.ts';
 import { Product } from '../../../interfaces/product.interfaces.ts';
 import { useEffect, useState } from 'react';
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
+import { MenuList } from './MenuList/MenuList.tsx';
 
 export function Menu(): JSX.Element {
   const [products, setProducts] = useState<Product[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | undefined>();
 
   const getMenu = async () => {
     try {
+      setIsLoading(true);
       //axios
+      await new Promise<void>((resolve) =>
+        setTimeout(() => {
+          resolve();
+        }, 2000),
+      );
       const { data } = await axios.get<Product[]>(`${API}/products`);
       setProducts(data);
+      setIsLoading(false);
     } catch (error) {
       console.error('Error:', error);
+
+      if (error instanceof AxiosError) {
+        setError(error.message);
+      }
+      setIsLoading(false);
+      return;
     }
   };
 
@@ -31,17 +46,9 @@ export function Menu(): JSX.Element {
         <Search placeholder="Enter a dish or composition" />
       </div>
       <div>
-        {products.map((product) => (
-          <ProductCart
-            key={product.id}
-            id={product.id}
-            title={product.name}
-            description={product.ingredients.join(', ')}
-            price={product.price}
-            rating={product.rating}
-            image={product.image}
-          />
-        ))}
+        {error && <div>{error}</div>}
+        {!isLoading && <MenuList products={products} />}
+        {isLoading && <div>Loading...</div>}
       </div>
     </>
   );
